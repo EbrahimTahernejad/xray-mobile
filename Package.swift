6fb3ea2fb1d215a914ef32c485032914fca15bf2
EbrahimTahernejad <EbrahimTahernejad@users.noreply.github.com>
2023-03-12 18:35:25 +0000

    XRay Release 1.8.1

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XRay",
-      url: "https://github.com/EbrahimTahernejad/xray-mobile/releases/download/1.8.0/XRay.xcframework.zip",
-      checksum: "152e6391680af986ce469bba8e7ee209bf65e300182ac0bb6cf2ff5ba5271520"
+      url: "https://github.com/EbrahimTahernejad/xray-mobile/releases/download/1.8.1/XRay.xcframework.zip",
+      checksum: "803a4561f614971744b044fe2943710025297cb6064f78824f55f7f9f1f46fb0"
     )
   ]
 )
